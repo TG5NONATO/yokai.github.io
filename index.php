4025be317255f6717ade510394877e8874bf346e
TG5NONATO <tg5nonato@gmail.com>
2025-08-25 20:09:36 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+  <!--- basic page needs
+  ================================================== -->
+  <meta charset="utf-8">
+	<title>Lineage II</title>
+	<meta name="description" content="Lineage II Coming soon">
+	<meta name="author" content="ver">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/base.css">
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/vendor.css">
+
+   <!-- script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- favicons
+	================================================== -->
+	<link rel="icon" type="image/png" href="images/favicon.ico">
+
+</head>
+
+<body id="top">
+
+	<!-- header
+   ================================================== -->
+   <header>
+
+   	<div class="row">
+   		<div class="logo" style="height: 56px; width: 268px;">
+	         <a href="#" style="background-size: 268px 56px; height: 56px; width: 268px;">Lineage II</a>
+	      </div>
+
+			<div class="social-links only-desktop">
+			   <ul>
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				   <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+				</ul>
+			</div>
+   	</div>
+
+   </header> <!-- /header -->
+
+   <!-- home
+   ================================================== -->
+   <section id="home" class="home-slides">
+
+   	<div class="shadow-overlay"></div>
+
+   	<div class="content-wrap-table">
+
+		   <div class="main-content-tablecell">
+
+		   	<div class="row">
+		   		<div class="col-twelve">
+
+		   			<div id="counter">
+		   				<div class="half">
+		   					<span>334 <sup>days</sup></span>
+		 						<span>23 <sup>hours</sup></span>
+		   				</div>
+							<div class="half">
+								<span>50 <sup>mins</sup></span>
+		 						<span>33 <sup>secs</sup></span>
+		 					</div>
+		   			</div>
+
+			  			<div class="bottom-text">
+			  				<h1>Our server is launching soon.</h1>
+				  			<p>
+    							We are working on server. </br>Subscribe to get notified when we launch!
+				  			</p>
+			  			</div>
+
+			   	</div> <!-- /twelve -->
+
+			   	<div class="scroll-icon">
+
+					   <p class="scroll-text">Scroll For More Info</p>
+					   <a href="#info" class="smoothscroll">
+					   	<div class="mouse"></div>
+					   </a>
+				   	<div class="end-top"></div>
+
+					</div> <!-- /scroll-icon -->
+		   	</div> <!-- /row -->
+
+		   </div> <!-- /main-content -->
+
+		</div> <!-- /content-wrap -->
+
+   </section> <!-- /home -->
+
+
+   <!-- info
+   ================================================== -->
+   <section id="info">
+
+   	<div class="info-overlay"></div>
+
+   	<div class="row">
+   		<div class="col-twelve tabs-wrap">
+
+   			<ul class="tabs">
+   				<li class="active" data-id="tab-about"><i class="icon-user"></i><span>About</span></li>
+					<li data-id="tab-subscribe"><i class="icon-newspaper"></i><span>Subscribe</span></li>
+				</ul> <!-- /tabs -->
+
+				<div class="tab-container">
+
+					<!-- tab content - about
+   				================================================== -->
+					<div id="tab-about" class="tab-content">
+
+					   <div class="tab-entry">
+
+					   	<div class="row tab-entry-intro">
+					   		<div class="col-twelve with-bottom-line">
+
+					   			<h1>Get to know a bit of our server</h1>
+
+					   			<p class="lead">Our server was developed with very accuracy to make our players happy!</p>
+
+					   		</div>
+					   	</div> <!-- /tab-content-intro -->
+
+					   	<div class="row about-content tab-entry-content">
+
+					   		<div class="about-list block-1-3 block-s-1-2 block-tab-full">
+
+						      	<div class="bgrid item">
+
+						      		<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+
+						            	 <h3 class="h05">Rates</h3>
+                            <p>
+                               <b>XP:</b> 50x <br>
+                               <b>SP:</b> 50x <br>
+                               <b>Spoil:</b> 5x <br>
+                               <b>Adena:</b> 10x <br>
+                               <b>Quest Items:</b> 3x <br>
+                             </p>
+
+						         	</div>
+
+									</div> <!-- /bgrid -->
+
+									<div class="bgrid item">
+
+										<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+						            	<h3 class="h05">Enchants</h3>
+
+                            <p>
+                              <b>Maximum Weapon:</b> +16 <br>
+                              <b>Maximum Armor:</b> +16 <br>
+                              <b>Maximum Jewel:</b> +16 <br>
+                              <b>Normal Scroll:</b> 65% <br>
+                              <b>Blessed Scroll:</b> 85% <br>
+                            </p>
+
+						            </div>
+
+								   </div> <!-- /bgrid -->
+
+								   <div class="bgrid item">
+
+								   	<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+						            	<h3 class="h05">Events</h3>
+
+                            <p>
+                              Team VS Team <br>
+                              Capture The Flag <br>
+  						        			</p>
+
+						            </div>
+
+								   </div> <!-- /bgrid -->
+
+									<div class="bgrid item">
+
+										<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+						            	<h3 class="h05">Weapons</h3>
+
+                            <p>
+                              No-Grade <br>
+                              Grade D <br>
+                              Grade C <br>
+                              Grade B <br>
+                              Grade A <br>
+                              Grade S	<br>
+                            </p>
+
+						            </div>
+
+									</div> <!-- /bgrid -->
+
+								   <div class="bgrid item">
+
+								   	<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+						            	<h3 class="h05">Armors</h3>
+
+                            <p>
+                              No-Grade <br>
+                              Grade D <br>
+                              Grade C <br>
+                              Grade B <br>
+                              Grade A <br>
+                              Grade S	<br>
+                            </p>
+
+						            </div>
+
+								   </div> <!-- /bgrid -->
+
+								   <div class="bgrid item">
+
+								   	<span class="icon"><i class="icon-star"></i></span>
+
+						            <div class="item-content">
+						            	 <h3 class="h05">Custom</h3>
+
+                             <p>
+                               No-Grade <br>
+                               Grade D <br>
+                               Grade C <br>
+                               Grade B <br>
+                               Grade A <br>
+                               Grade S	<br>
+                             </p>
+
+                       </div>
+
+								   </div> <!-- /bgrid -->
+
+						      </div> <!-- /about-list -->
+
+					     	</div> <!-- /row about-content -->
+
+					   </div> <!-- /tab-entry -->
+
+					</div> <!-- /tab-about -->
+
+
+					<!-- tab content - subscribe
+   				================================================== -->
+					<div id="tab-subscribe" class="tab-content">
+
+						<div class="tab-entry">
+
+							<div class="row tab-entry-intro">
+					   		<div class="col-twelve with-bottom-line">
+
+					   			<h1>Subcribe to get notified when our server launch!</h1>
+
+					   			<p class="lead">Don't worry, we don't like spam and we will not send spam to anyone.</p>
+
+					   		</div>
+					   	</div> <!-- /tab-content-intro -->
+
+					   	<div class="row form-wrap tab-entry-content">
+					   		<div class="col-twelve">
+
+					   			<form id="mc-form" class="group" novalidate="true">
+
+										<div class="form-field">
+						   				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+						   			</div>
+
+							   		<div class="form-field">
+							   			<input type="submit" value="Get Notified" name="subscribe" class="button button-primary large">
+							   		</div>
+
+							   		<label for="mce-EMAIL" class="subscribe-message"></label>
+
+									</form>
+
+					   		</div> <!-- /twelve -->
+					   	</div> <!-- subscribe-form -->
+
+						</div> <!-- /tab-entry -->
+
+					</div> <!-- /tab-subscribe -->
+
+				</div> <!-- /tab-container -->
+
+   		</div> <!-- twelve -->
+   	</div> <!-- row -->
+
+   </section> <!-- /info -->
+
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+   	<div class="social-wrap">
+   		<div class="row">
+
+	         <ul class="footer-social-list">
+	            <li><a href="#">
+	             	<i class="fa fa-facebook"></i><span>Facebook</span>
+	            </a></li>
+	            <li><a href="#">
+	              	<i class="fa fa-twitter"></i><span>Twitter</span>
+	            </a></li>
+	            <li><a href="#">
+	              	<i class="fa fa-instagram"></i><span>Instagram</span>
+	            </a></li>
+	         </ul>
+
+			</div> <!-- /row -->
+   	</div> <!-- /social-wrap -->
+
+   	<div class="footer-bottom">
+
+			<div class="footer-logo" style="height: 56px; width: 268px;">
+					<img src="images/logox.png" alt="">
+			</div>
+
+   		<div class="copyright">
+		     	<span>© Copyright MyServer 2017.</span>
+		     	<span>Design by <a href="https://www.l2jbrasil.com/index.php?/user/5328-vert/">vert</a></span>
+		   </div>
+
+   	</div> <!-- /footer-bottom -->
+
+   	<div class="back-to-top">
+		 	<a href="#top" class="smoothscroll"><span>Back to Top</span></a>
+		</div>
+
+   </footer>
+
+   <div id="preloader">
+    	<div id="loader"></div>
+   </div>
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="js/jquery-2.1.3.min.js"></script>
+   <script src="js/plugins.js"></script>
+   <script src="js/main.js"></script>
+
+</body>
+
+</html>
